edc35479260aefd7d6ac484a0b9980721b35cbb9
jnc3designs <jnc3designs@gmail.com>
2026-05-13 00:30:13 -0500

    Update page.js

--- jnc3designs-premium/app/page.js
+++ jnc3designs-premium/app/page.js
@@ -235,7 +235,7 @@ export default function Home() {
           <button type="submit" className="button-primary">Send Request</button>
         </form>
     <p style={{ fontSize: "12px", opacity: 0.6, marginTop: "10px" }}>
-  By submitting a request, you agree to our{" "}
+  Custom and industrial parts are customer-approved for intended use. By submitting a request, you agree to our{" "}
   <a href="/policies" className="underline">
     Shop Policies
   </a>.
