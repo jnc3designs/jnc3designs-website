47c2f993dc6fc4e2fc157f8ecc6013782865f8c3
jnc3designs <jnc3designs@gmail.com>
2026-07-11 20:52:50 -0500

    Create page.js

---
+++ jnc3designs-premium/app/operations/inventory/page.js
@@ -0,0 +1,19 @@
+import InventorySummary from "../../../components/InventorySummary";
+import OperationsHeader from "../../../components/OperationsHeader";
+
+export default function InventoryPage() {
+  return (
+    <main>
+      <OperationsHeader
+        badge="🧵 Operations Module"
+        badgeColor="green"
+        title="Inventory"
+        description="Monitor current filament quantities and quickly identify products that need to be restocked."
+        backHref="/operations"
+        backLabel="Operations"
+      />
+
+      <InventorySummary />
+    </main>
+  );
+}
