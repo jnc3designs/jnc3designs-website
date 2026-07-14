400516ee7b4f4e4755ec76982cf28b541eb7fdbf
jnc3designs <jnc3designs@gmail.com>
2026-07-13 20:53:12 -0500

    Create activity.js

---
+++ jnc3designs-premium/data/activity.js
@@ -0,0 +1,30 @@
+export const activity = [
+  {
+    id: 1,
+    time: "8:15 AM",
+    icon: "🖨️",
+    title: "P1S started printing",
+    description: "Demo Industrial Adapter",
+  },
+  {
+    id: 2,
+    time: "8:42 AM",
+    icon: "📦",
+    title: "Rush order updated",
+    description: "JNC-1002 moved to Printing",
+  },
+  {
+    id: 3,
+    time: "9:05 AM",
+    icon: "🧵",
+    title: "Inventory updated",
+    description: "ASA Black stock adjusted",
+  },
+  {
+    id: 4,
+    time: "9:20 AM",
+    icon: "👥",
+    title: "Customer added",
+    description: "New customer profile created",
+  },
+];
